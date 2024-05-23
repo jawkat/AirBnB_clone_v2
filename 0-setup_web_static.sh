29ea806b56f78b4a582e418b15431adf07027d95
jawkat <kad.joliano@gmail.com>
2024-05-23 11:53:59 +0100

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# Bash script that sets up your web servers for the deployment of web_static
+# run script on both web servers.
+# checks if Nginx is not installed or not executable
+sudo apt update
+sudo apt -y install nginx
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+  # Create necessary folders
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration to serve /data/web_static/current/ at /hbnb_static
+echo "server {
+    listen 80;
+    server_name _;
+         root /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+    location / {
+        # Your other configuration directives, if any
+    }
+}" > /etc/nginx/sites-available/default
+
+# sudo nginx -t
+sudo service nginx restart
